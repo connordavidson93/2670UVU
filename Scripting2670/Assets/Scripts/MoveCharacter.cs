e5bfb0bac44421d397e0e64f319ba9f127cff147
Unknown <connordavidson93@gmail.com>
2017-08-28 14:04:52 -0600

    Working with Actions

---
+++ Scripting2670/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour {
+
+	CharacterController cc;
+	Vector3 tempMove;
+	public float speed = 5;
+
+	void Start () {
+		cc = GetComponent<CharacterController>();
+		MoveInput.KeyAction = Move;
+	}
+	
+	void Move (float _movement) {
+		print(_movement);
+		tempMove.x = _movement*speed*Time.deltaTime;
+	}
+}
