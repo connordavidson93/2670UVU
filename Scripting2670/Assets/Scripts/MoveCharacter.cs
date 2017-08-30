be5bc02a5fa109e95972cfc9e09d225115866d65
Unknown <connordavidson93@gmail.com>
2017-08-30 14:01:10 -0600

    Animate and Flip to Actions

--- Scripting2670/Assets/Scripts/MoveCharacter.cs
+++ Scripting2670/Assets/Scripts/MoveCharacter.cs
@@ -6,15 +6,15 @@ public class MoveCharacter : MonoBehaviour {
 
 	CharacterController cc;
 	Vector3 tempMove;
-	public float speed = 5;
+	public float speed = 3;
 
 	void Start () {
 		cc = GetComponent<CharacterController>();
-		MoveInput.KeyAction = Move;
+		MoveInput.KeyAction += Move;
 	}
 	
 	void Move (float _movement) {
-		print(_movement);
 		tempMove.x = _movement*speed*Time.deltaTime;
+		cc.Move(tempMove);
 	}
 }
