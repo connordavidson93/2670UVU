894a4083f08dece8cfa2b1bb310f129e1e880306
Unknown <connordavidson93@gmail.com>
2017-09-01 11:46:35 -0600

    Working on Objects in Prototype

---
+++ Side Scroll Prototype/Bear Prototypes/Assets/MoveCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour {
+
+	CharacterController cc;
+	Vector3 tempMove;
+	public float speed = 3;
+
+	void Start () {
+		cc = GetComponent<CharacterController>();
+		MoveInput.KeyAction += Move;
+	}
+	
+	void Move (float _movement) {
+		tempMove.x = _movement*speed*Time.deltaTime;
+		cc.Move(tempMove);
+	}
+}
