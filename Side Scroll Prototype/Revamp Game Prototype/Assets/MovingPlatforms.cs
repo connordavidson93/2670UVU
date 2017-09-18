1dce1e16fc047f1ba93e93c882f8e17ca4e87d1c
Unknown <connordavidson93@gmail.com>
2017-09-18 15:18:03 -0600

    Created a working puzzle and fixed elements of the game

---
+++ Side Scroll Prototype/Revamp Game Prototype/Assets/MovingPlatforms.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MovingPlatforms : MonoBehaviour {
+	public Transform MovingPlatform;
+	public Transform Position1;
+	public Transform Position2;
+	public Vector3 NewPosition;
+	public string CurrentState;
+	public float Travel;
+	public float ResetTime;
+
+
+
+	void Start () {
+		ChangePosition();
+	}
+	
+	void FixedUpdate () {
+		MovingPlatform.position = Vector3.Lerp (MovingPlatform.position, NewPosition, Travel*Time.deltaTime);
+	}
+
+	void ChangePosition (){
+		if(CurrentState == "Moving to Position 1"){
+			CurrentState = "Moving to Position 2";
+			NewPosition = Position2.position;
+		}
+		else if(CurrentState == "Moving to Position 2"){
+			CurrentState = "Moving to Position 1";
+			NewPosition = Position1.position;
+		}
+		else if(CurrentState == ""){
+			CurrentState = "Moving to Position 2";
+			NewPosition = Position2.position;
+		}
+		Invoke("ChangePosition", ResetTime);
+	}
+}
