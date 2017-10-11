b283f0b12dbb626ace5af5adb5f4d37c0153cbca
Unknown <connordavidson93@gmail.com>
2017-10-11 14:19:24 -0600

    Creating Player Preference Scripts for later use

---
+++ Side Scroll Prototype/Revamp Game Prototype/Assets/Scripts/PlayerPrefs/SetGameData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SetGameData : MonoBehaviour {
+	public Data data;
+	void OnApplicationQuit()
+	{
+		PlayerPrefs.SetString("GameData", JsonUtility.ToJson(data));
+	}
+}
